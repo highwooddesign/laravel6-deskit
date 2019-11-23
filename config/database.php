e5161578053e853ef0888411c2328da2fac16205
Dave Horsfall <dave@horsfall.dev>
2019-11-23 10:08:12 +0000

    Travis CI setup

--- config/database.php
+++ config/database.php
@@ -97,6 +97,18 @@ return [
             'prefix_indexes' => true,
         ],
 
+
+'testing' => [
+  'driver' => 'mysql',
+  'host' => env('DB_TEST_HOST', 'localhost'),
+  'database' => env('DB_TEST_DATABASE', 'homestead'),
+  'username' => env('DB_TEST_USERNAME', 'homestead'),
+  'password' => env('DB_TEST_PASSWORD', 'secret'),
+  'charset' => 'utf8',
+  'collation' => 'utf8_unicode_ci',
+  'prefix' => '',
+  'strict' => false,
+],        
     ],
 
     /*
@@ -150,15 +162,4 @@ return [
 
     ],
 
-'testing' => [
-  'driver' => 'mysql',
-  'host' => env('DB_TEST_HOST', 'localhost'),
-  'database' => env('DB_TEST_DATABASE', 'homestead'),
-  'username' => env('DB_TEST_USERNAME', 'homestead'),
-  'password' => env('DB_TEST_PASSWORD', 'secret'),
-  'charset' => 'utf8',
-  'collation' => 'utf8_unicode_ci',
-  'prefix' => '',
-  'strict' => false,
-],    
 ];
